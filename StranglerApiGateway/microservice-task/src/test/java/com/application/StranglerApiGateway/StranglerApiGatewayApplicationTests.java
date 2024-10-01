bcd7fa5060fb0ed56010e14e57c618821f0c6c3d
Gvln <santis.paez2414@outlook.com>
2024-10-01 13:10:44 -0500

    microservices: v0.1.0

--- StranglerApiGateway/microservice-task/src/test/java/com/application/StranglerApiGateway/StranglerApiGatewayApplicationTests.java
+++ StranglerApiGateway/microservice-task/src/test/java/com/application/StranglerApiGateway/StranglerApiGatewayApplicationTests.java
@@ -1,13 +1,10 @@
 package com.application.StranglerApiGateway;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StranglerApiGatewayApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
 
 }
